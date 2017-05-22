e0ecf60e652b1d2586ce09915bcf6b7f494bad62
lukasgalas <lukas@example.com>
2017-05-22 10:26:24 +0700

    xxx_add new theme

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -50,5 +50,15 @@ module.exports = {
             'css/styles'
         ],
         dsl: 'less'
+    },
+    customtheme: {
+        area: 'adminhtml',
+        name: 'Custom/customTheme',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
     }
 };
